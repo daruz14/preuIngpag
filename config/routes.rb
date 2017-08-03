b30054aa4321a2207b5e41fa7d64be71aa968b90
David <daruz@uc.cl>
2017-08-03 16:42:46 -0400

    change views and flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :news
   resources :lessons
   resources :courses
   devise_for :users
   resources :users
-  root 'users#index'
+  get 'home', to: 'news#home'
+  root 'news#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802051332) do
+ActiveRecord::Schema.define(version: 20170803194136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20170802051332) do
     t.datetime "updated_at", null: false
     t.integer  "course_id"
     t.index ["course_id"], name: "index_lessons_on_course_id", using: :btree
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
